b96671706ed59289701729d03da360c7ed7253a1
YariLAN <67001313+YariLAN@users.noreply.github.com>
2023-04-12 23:26:32 +0300

    Delete User.cs

--- SystemHotel/Domain/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SystemHotel.Domain.Entities
-{
-    public class User
-    {
-        public int User_Id { get; set; }
-        public string login { get; set; }
-        public string pass { get; set; }
-    }
-}
